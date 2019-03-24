a37437d62132511a46ede3706a0def42beb17e27
Justin Caswell <jcaswell@mlla3714.magicleap.ds>
2019-03-24 19:27:40 -0400

    added assignment args

--- init.py
+++ init.py
@@ -1,9 +1,46 @@
-import os
-import sys,subprocess
+import os,sys,getopt
 
-try:
-  subprocess.call(["ps","aux"])
-  print("")
-  os.system("free -m")
-except:
-  print("error")
+def args():
+    fullCmdArguments = sys.argv
+
+    # - further arguments
+    argumentList = fullCmdArguments[1:]
+
+
+    unixOptions = "htp"
+    gnuOptions = ["help","test","production"]
+
+    try:
+        arguments, values = getopt.getopt(argumentList, unixOptions, gnuOptions)
+    except getopt.error as err:
+        # output error, and return with an error code
+        print (str(err))
+        sys.exit(2)
+
+    for currentArgument, currentValue in arguments:
+        if currentArgument in ("-t", "--test"):
+            testScript = currentValue
+            stageScript()
+        elif currentArgument in ("-h", "--help"):
+            print("-t test script before executing in production\n-h prints the help menu")
+            exit()
+        elif currentArgument in ("-p","--production"):
+            prodScript()
+        else:
+            print("option not valid")
+
+
+
+def stageScript():
+    print("This is staging")
+    os.system("free -m")
+    os.system("df -h")
+
+def prodScript():
+    print("This is production")
+
+def main():
+    args()
+
+
+main()
